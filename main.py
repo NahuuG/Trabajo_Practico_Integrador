8c142c11e6d68110dee50b58a42fac400934af15
NahuuG <nahuel.invicto922@gmail.com>
2023-10-23 13:20:41 -0300

    Se agregan las opciones que puede hacer un estudiante

--- main.py
+++ main.py
@@ -44,14 +44,14 @@ def menu_alumno(estudiante):
         opcion = input("Ingrese su opción: ")
 
         if opcion == "1":
-            "matricularse_a_curso(estudiante)"
+            matricularse_a_curso(estudiante)
         elif opcion == "2":
-            "ver_cursos_matriculados(estudiante)"
+            ver_cursos_matriculados(estudiante)
         elif opcion == "3":
-            print("Volviendo al menú principal.")
+            print("Volviendo al menú principal....")
             break
         else:
-            print("Opción inválida. Por favor, ingrese una opción válida.")
+            print("Opción inválida.")
 
 
 # Menu para PROFESORES
@@ -95,3 +95,63 @@ def informes():
             break
         else:
             print("Opción inválida. Por favor, ingrese una opción válida.")
+            
+
+# Funcion para que el alumno ingrese al sistema
+def ingresar_como_alumno(email): 
+    # Recorre en la lista estudiantes a cada estudiante 
+    for estudiante in lista_estudiantes:
+        if estudiante.mail == email:
+            password = input("Ingrese su contraseña: ")
+            if Usuario.validar_credenciales(email, password):
+                menu_alumno(estudiante)
+                return
+            else:
+                print("Contraseña incorrecta. Por favor, inténtelo de nuevo.")
+                return
+    # Si no se encontro ningun alumno con ese mail se le pregunta si quiere darse de alta
+    confirmacion = input("No se encontró un estudiante con ese email. ¿Desea darlo de alta? (si/no): ").lower()
+    if confirmacion == "si":
+        alta_alumno(email)
+    else:
+        print("Operacion cancelada.")
+
+
+# Esta funcion nos muestra a los Cursos que esta o no inscripto el estudiante
+def ver_cursos_matriculados(estudiante):
+    if not estudiante.mi_cursos:
+        print("No estás matriculado en ningún curso aún.")
+    else:
+        print("\n Cursos Matriculados ")
+        for i, curso in enumerate(estudiante.mi_cursos, start=1):
+            print(f"{i}. {curso.nombre}")
+
+
+# Da la posibilidad a inscribirse a un curso
+def matricularse_a_curso(estudiante):
+    lista_cursos_ordenados = sorted(lista_cursos, key=lambda cursos: cursos.nombre)
+    ver_cursos(lista_cursos)
+    opcion = input("Seleccione el número del curso al que desea matricularse: ")
+    if opcion.isdigit():
+        curso_index = int(opcion) - 1
+        if 0 <= curso_index < len(lista_cursos_ordenados):
+            curso = lista_cursos_ordenados[curso_index]
+            contrasenia = input(f"Ingrese la contraseña de matriculación para el curso {curso.nombre}: ")
+            mensaje = estudiante.matricularse_al_curso(curso, contrasenia)
+            print(mensaje)           
+        else:
+            print("Opción inválida. Por favor, ingrese una opción válida.")
+    else:
+        print("Opción inválida. Por favor, ingrese un número válido.")
+
+        
+# Se encarga de dar de alta a un alumno en el sistema
+def alta_alumno(mail:str):
+    nombre = input("Ingrese el nombre: ")
+    apellido = input("Ingrese el apellido: ")
+    legajo = input("Ingrese el numero de legajo: ")
+    password = input("Ingrese la contraseña: ")
+    fecha_actual = datetime.datetime.now()
+    fecha_inscripcion = fecha_actual.year
+    nuevo_estudiante = Estudiante(nombre, apellido, mail, password, legajo, fecha_inscripcion)
+    lista_estudiantes.append(nuevo_estudiante)
